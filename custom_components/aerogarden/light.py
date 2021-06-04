dd1c8983429358e0fc3c93aa352e15dc663c3e89
GoPosi <44408784+GoPosi@users.noreply.github.com>
2021-06-04 16:39:06 -0400

    Update light.py

--- custom_components/aerogarden/light.py
+++ custom_components/aerogarden/light.py
@@ -13,7 +13,7 @@ DEPENDENCIES = ["aerogarden"]
 UPDATE_STATE_FROM_CLOUD = True
 
 
-class AerogardenLight(Light):
+class AerogardenLight(LightEntity):
     def __init__(self, macaddr, aerogarden_api, field="lightStat", label="Light"):
         self._aerogarden = aerogarden_api
         self._macaddr = macaddr
